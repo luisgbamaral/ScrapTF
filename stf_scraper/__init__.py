af620e76ef82498f69a44b4ed1635bb11487445f
Luís Guilherme B. Amaral <127261157+luisgbamaral@users.noreply.github.com>
2025-07-13 12:40:18 -0300

    Update __init__.py
    change e expand exceptions

--- stf_scraper/__init__.py
+++ stf_scraper/__init__.py
@@ -1,9 +1,20 @@
-"""STF Scraper - Biblioteca para extração de dados do Supremo Tribunal Federal."""
+"""ScrapTF - Biblioteca para extração de dados do Supremo Tribunal Federal.
+
+Biblioteca otimizada para coleta automatizada de processos judiciais do STF
+com técnicas anti-bloqueio e processamento em larga escala.
+"""
 
 __version__ = "2.0.0"
-__author__ = "STF Scraper Team"
+__author__ = "ScrapTF Team"
 
 from .core.scraper import STFScraper
 from .utils.validators import CNJValidator
+from .exceptions import ScrapTFError, ValidationError, NetworkError
 
-__all__ = ["STFScraper", "CNJValidator"]
+__all__ = [
+    "STFScraper",
+    "CNJValidator", 
+    "ScrapTFError",
+    "ValidationError",
+    "NetworkError"
+]
